832743a1d8f75d1fdb96996ee0d547d573459a4e
FranciscoCastillo <castillofranartur1996@gmail.com>
2024-07-20 14:11:45 -0500

    Se agregaron las dos vistas faltantes Registro y Categorias, ademas todas las pantallas solicitadas por el reto son navegables, pendiente modificar algunas cosas. Se eliminaron algunas cosas que habia hecho Samuel para facilitar la navegacion.

--- app/src/main/java/com/example/solarsport/StatisticsActivity.java
+++ app/src/main/java/com/example/solarsport/StatisticsActivity.java
@@ -1,6 +1,9 @@
 package com.example.solarsport;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,45 +13,51 @@ import java.util.Collections;
 
 public class StatisticsActivity extends AppCompatActivity {
 
-    private TextView tvAverageEnergy, tvMaxEnergy, tvMinEnergy;
-    private ArrayList<Integer> energyData; // Lista que contendrá datos de energía para cálculo de estadísticas
+    private ImageButton shomeb, scategoriesb, sstatisticsb, sbenefitsb, sprofileb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_activity);
 
-        tvAverageEnergy = findViewById(R.id.tvAverageEnergy);
-        tvMaxEnergy = findViewById(R.id.tvMaxEnergy);
-        tvMinEnergy = findViewById(R.id.tvMinEnergy);
+        shomeb=findViewById(R.id.shomeb);
+        scategoriesb=findViewById(R.id.scategoriesb);
+        sstatisticsb=findViewById(R.id.sstatisticsb);
+        sbenefitsb=findViewById(R.id.sbenefitsb);
+        sprofileb=findViewById(R.id.sprofileb);
 
-        // Supongamos que energyData se llena con datos reales de la base de datos o algún servicio
-        energyData = new ArrayList<>();
-        energyData.add(100);
-        energyData.add(200);
-        energyData.add(150);
-        energyData.add(300);
-        energyData.add(250);
+        shomeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StatisticsActivity.this, MainActivity.class));
+            }
+        });
+        scategoriesb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StatisticsActivity.this, CategoriesActivity.class));
+            }
+        });
+        sstatisticsb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StatisticsActivity.this, StatisticsActivity.class));
+            }
+        });
+        sbenefitsb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StatisticsActivity.this, BenefitsActivity.class));
+            }
+        });
+        sprofileb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StatisticsActivity.this, ProfileActivity.class));
+            }
+        });
 
-        // Calcular estadísticas
-        calculateStatistics();
-    }
 
-    private void calculateStatistics() {
-        if (energyData.isEmpty()) return;
-
-        int sum = 0;
-        for (int energy : energyData) {
-            sum += energy;
-        }
-
-        int average = sum / energyData.size();
-        int max = Collections.max(energyData);
-        int min = Collections.min(energyData);
-
-        tvAverageEnergy.setText("Promedio de Energía Producida: " + average + " kWh");
-        tvMaxEnergy.setText("Máxima Energía Producida: " + max + " kWh");
-        tvMinEnergy.setText("Mínima Energía Producida: " + min + " kWh");
     }
 }
 
